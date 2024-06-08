202a3043ee11b2251d2372e9fdf667bdc8050134
Mahesh-KuntaMK <k.maheshkunta@gmail.com>
2024-06-08 16:51:52 +0530

    added database credentails

--- util/database.js
+++ util/database.js
@@ -1,13 +1,9 @@
 const Sequelize=require('sequelize');
 
-// const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-//     host:process.env.DB_HOST,
-//     dialect: 'mysql',
-//     timezone: '+05:30'
-// })
-const sequelize=new Sequelize('expense-project','root','Mahesh@1313',{
-    host:'3.82.4.129',
+const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+    host:process.env.DB_HOST,
     dialect: 'mysql',
     timezone: '+05:30'
 })
+
 module.exports = sequelize;
