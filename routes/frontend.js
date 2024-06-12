c4ff75e890d76410fc3fbe5c89e21c7e838aaa54
Mahesh-KuntaMK <k.maheshkunta@gmail.com>
2024-06-12 13:20:33 +0530

    ready to deploy  in aws server

--- routes/frontend.js
+++ routes/frontend.js
@@ -10,9 +10,9 @@ const frontendController=require('../controllers/frontendController.js');
 routes.get('/',frontendController.signup);
 routes.get('/user/createaccount',frontendController.createaccount);
 routes.post('/user/signup',frontendController.usersignup);
-routes.get('/user/loginpage',frontendController.login);
+//routes.get('/user/loginpage',frontendController.login);
 routes.post('/user/login',frontendController.userlogin);
-routes.get('/user/login',frontendController.login);
+//routes.get('/user/login',frontendController.login);
 routes.get('/password/forgotpassword',frontendController.forgotPassword);
 
 routes.post('/password/forgotpassword',frontendController.resetPassword);
