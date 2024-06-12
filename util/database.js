c246af704528a21da7dfc65fd5970c9b049f66a6
Mahesh-KuntaMK <k.maheshkunta@gmail.com>
2024-06-12 13:27:01 +0530

    added database aws

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const Sequelize=require('sequelize');
 
-// const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
-//     host:process.env.DB_HOST,
-//     dialect: 'mysql',
-//     timezone: '+05:30'
-// })
+const sequelize=new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{
+    host:process.env.DB_HOST,
+    dialect: 'mysql',
+    timezone: '+05:30'
+})
 
 
 module.exports = sequelize;
